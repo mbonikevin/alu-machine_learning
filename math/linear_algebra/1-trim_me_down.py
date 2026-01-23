c5e9a0161600786cf890a91ed42555aa5e9c604d
mbonikevin <k.mbonimpay@alustudent.com>
2026-01-23 10:26:47 +0200

    for loop fix

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""the module that trims the matrix"""
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
-for row in matrix: the_middle.append(row[2:4])
-print("The middle columns of the matrix are: {}".format(the_middle))
+for row in matrix: 
+    the_middle.append(row[2:4])
+    print("The middle columns of the matrix are: {}".format(the_middle))
