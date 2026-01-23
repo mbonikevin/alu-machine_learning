25ce7dfe74e7eeae4dc4e7f25ebee2adfe8555ae
mbonikevin <k.mbonimpay@alustudent.com>
2026-01-23 10:45:18 +0200

    12 import

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 """the module that defines np_elementwise function"""
 
-import numpy as np
-
 
 def np_elementwise(mat1, mat2):
     """performing element-wise addition, subtraction, multiplication, and division"""
-    mat1 = np.array(mat1)
-    mat2 = np.array(mat2)
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
