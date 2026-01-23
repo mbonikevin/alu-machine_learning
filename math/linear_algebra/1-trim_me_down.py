429fecb9e2a47e75c1b543fa3b1e8947ea162687
mbonikevin <k.mbonimpay@alustudent.com>
2026-01-23 10:23:19 +0200

    fix

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""the module that trims the matrix"""
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
 for row in matrix: the_middle.append(row[2:4])
