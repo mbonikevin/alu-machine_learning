04518047a87156f4536ac22b02bb5c4ac0f18094
mbonikevin <k.mbonimpay@alustudent.com>
2026-02-15 15:03:57 +0200

    fix must not be python

--- math/calculus/9-sum_total.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""summation of i squared"""
-
-
-def summation_i_squared(n):
-    """calculate sum of i^2 from 1 to n"""
-    if not isinstance(n, int) or n < 1:
-        return None
-    return n * (n + 1) * (2 * n + 1) // 6
