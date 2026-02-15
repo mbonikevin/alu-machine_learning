db5fda89422c5164345292fcea239b391ebfca98
mbonikevin <k.mbonimpay@alustudent.com>
2026-02-15 15:35:03 +0200

    fix/17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,7 +4,7 @@
 
 def poly_integral(poly, C=0):
     """calculate the integral of a polynomial"""
-    if not isinstance(poly, list) or not isinstance(C, int):
+    if not isinstance(poly, list) or not isinstance(C, int) or poly == []:
         return None
 
     result = [C]
